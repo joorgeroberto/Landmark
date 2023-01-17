d529db980635f7bc4759e5bb85bbd62393183918
Jorge de Carvalho <jjunio63@ford.com>
2023-01-17 15:37:03 -0300

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Jorge de Carvalho on 17/01/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
