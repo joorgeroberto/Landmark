bec07730966dacd5a31df482fc91ce8dcafb8185
Jorge Roberto <jorgercj@dcomp.ufs.br>
2023-01-23 14:01:35 -0300

    Add a Category View

---
+++ Landmark/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmark
+//
+//  Created by Jorge de Carvalho on 23/01/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
