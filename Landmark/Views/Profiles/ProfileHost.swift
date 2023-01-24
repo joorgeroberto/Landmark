69d7f96ade84376cc610c1291ea433c23228ad73
Jorge de Carvalho <jorgercj@dcomp.ufs.br>
2023-01-24 16:23:10 -0300

    Display a User Profile

---
+++ Landmark/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmark
+//
+//  Created by Jorge de Carvalho on 24/01/23.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
